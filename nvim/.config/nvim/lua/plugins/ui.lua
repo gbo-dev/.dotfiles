1339e61269aed6251becbec5ee3bac179319165d
gbo <70324103+gbo-dev@users.noreply.github.com>
2025-06-22 01:04:07 +0200

    feat: use relative path of current file in lualine

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -23,6 +23,14 @@ return {
           component_separators = " ",
           section_separators = "",
         },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1, -- 0: just filename, 1: relative path, 2: absolute path
+            }
+          }
+        }
       }
     end
   }
